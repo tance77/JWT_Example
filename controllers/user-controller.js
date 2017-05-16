3b4f3a75d5f6c0e3e3d341e6a91e0d620a6d06dd
Lance Thompson <starrazor31@gmail.com>
2017-05-16 16:57:15 -0700

    Adding vue for login form

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,21 +4,32 @@ var salt   = bcrypt.genSaltSync(10);
 var jwt    = require("jsonwebtoken");
 
 module.exports.createUser = function (req, res) {
-    var password = bcrypt.hashSync(req.body.user_password, salt);
-    var sql      = "INSERT INTO users (username, user_password, email) VALUES('" + req.body.username + "', '" + password + "', '" + req.body.email + "')";
-
-    db.query(sql, function (err, rows, fields) {
+    var password  = bcrypt.hashSync(req.body.password, salt),
+        email     = req.body.email,
+        username  = req.body.username,
+        firstName = req.body.firstName,
+        lastName  = req.body.lastName;
+    db.query("INSERT INTO users SET username=?, password=?, email=?, first_name=?, last_name=?", [username, password, email, firstName, lastName], function (err, rows, fields) {
         if (err) {
-            res.status(500).send(err);
+            console.log(err);
+            res.status(409);
+            res.json({
+                error: true,
+                errorCode: 409,
+                errorMessage: "Duplicate User",
+                errorDescription: "This user already exists and could not be created"
+            });
         }
         else {
-            res.status(200).send("User was successfully created!");
+            res.json({
+                status: "User created successfully."
+            });
         }
     });
 };
 
 module.exports.login = function (req, res) {
-    var secret = "2a$10$aWC05XXmsto7PCnQxgO9f.Rnm0OYPYhaCqpT2dsX3.aCTJtr25eT6";
+    var secret            = "2a$10$aWC05XXmsto7PCnQxgO9f.Rnm0OYPYhaCqpT2dsX3.aCTJtr25eT6";
     var submittedPassword = req.body.user_password;
     var sql               = "SELECT * FROM users WHERE username='" + req.body.loginName + "' or email='" + req.body.loginName + "'";
     db.query(sql, function (err, rows, fields) {
@@ -27,7 +38,7 @@ module.exports.login = function (req, res) {
             var isVerified = bcrypt.compareSync(submittedPassword, userData.user_password);
             if (isVerified) {
                 var token = jwt.sign(userData, secret, {
-                    expiresIn: 60*60*24
+                    expiresIn: 60 * 60 * 24
                 });
                 delete userData.user_password;
                 res.json({
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
 var express = require("express");
 var router  = express.Router();
 
-
-
 /* GET home page. */
 router.get("/", function (req, res, next) {
-//    db.connection.connect();
-//    db.connection.query("SELECT 1 + 1 AS solution", function (err, rows, fields) {
-//        if (err) throw err;
-//        console.log("The solution is: " + rows[0].solution);
-//    });
-//    db.connection.end();
     res.render("index", {title: "Welcome"});
 });
-
 
 module.exports = router;
