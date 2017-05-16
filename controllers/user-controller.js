6a628f595ea24f7c4690e63415ac5760ca860063
Lance Thompson <lancet@myoutdesk.com>
2017-05-16 12:13:46 -0700

    adding JWT token to login

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,11 @@
 var db     = require("../database");
 var bcrypt = require("bcryptjs");
 var salt   = bcrypt.genSaltSync(10);
+var jwt    = require("jsonwebtoken");
 
 module.exports.createUser = function (req, res) {
     var password = bcrypt.hashSync(req.body.user_password, salt);
-    var sql    = "INSERT INTO users (username, user_password, email) VALUES('" + req.body.username + "', '" + password + "', '" + req.body.email + "')";
+    var sql      = "INSERT INTO users (username, user_password, email) VALUES('" + req.body.username + "', '" + password + "', '" + req.body.email + "')";
 
     db.query(sql, function (err, rows, fields) {
         if (err) {
@@ -17,23 +18,28 @@ module.exports.createUser = function (req, res) {
 };
 
 module.exports.login = function (req, res) {
+    var secret = "2a$10$aWC05XXmsto7PCnQxgO9f.Rnm0OYPYhaCqpT2dsX3.aCTJtr25eT6";
     var submittedPassword = req.body.user_password;
-    var sql = "SELECT * FROM users WHERE username='" + req.body.loginName + "' or email='" + req.body.loginName + "'";
+    var sql               = "SELECT * FROM users WHERE username='" + req.body.loginName + "' or email='" + req.body.loginName + "'";
     db.query(sql, function (err, rows, fields) {
         if (!err && rows.length > 0) {
-            var userData = rows[0];
+            var userData   = rows[0];
             var isVerified = bcrypt.compareSync(submittedPassword, userData.user_password);
-            if(isVerified){
+            if (isVerified) {
+                var token = jwt.sign(userData, secret, {
+                    expiresIn: 60*60*24
+                });
                 res.json({
+                    jwt_token: token,
                     data: userData
                 });
             }
-            else{
-                res.status(400).send("Incorrect password.")
+            else {
+                res.status(400).send("Incorrect password.");
             }
         }
-        else{
-            res.status(500).send("Unable to process query.")
+        else {
+            res.status(500).send("Unable to process query.");
         }
     });
 };
